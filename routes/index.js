819e04bc68098362bd4c724dea84abd9f1a5486d
TyrkiskPeber <77719459+TyrkiskPeber@users.noreply.github.com>
2023-01-20 10:35:10 +0100

    ujkhjikhu

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const mysql = require('mysql2');
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+});
+const promisePool = pool.promise();
+
 router.get('/', async function (req, res) {
     res.send('Hello you!')
 });
